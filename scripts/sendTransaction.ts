c4514ecbcdbdda7aac00aea25dc295392378416c
lishuailibertine <libertine9458@gmail.com>
2025-04-25 14:52:52 +0800

    init

---
+++ scripts/sendTransaction.ts
@@ -0,0 +1,3 @@
+import {AuthorizationListItem, EOACodeEIP7702Transaction } from '@ethereumjs/tx'
+import {bytesToHex, hexToBytes, type PrefixedHexString} from '@ethereumjs/util'
+import { ethers } from 'ethers'
